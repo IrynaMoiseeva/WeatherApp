5d543ab8c869381167d0e3fc19f96a1f1d794d0f
Irina Moiseeva <irynamoiseeva@gmail.com>
2019-03-10 23:38:49 +0200

    correction

--- MvxForms.Core/ViewModels/MvxFormsViewModel.cs
+++ MvxForms.Core/ViewModels/MvxFormsViewModel.cs
@@ -5,107 +5,93 @@ using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using Newtonsoft.Json;
 
-
 namespace MvxForms.Core.ViewModels
 {
     public class MvxFormsViewModel : MvxViewModel
     {
-        const string text = "Today rain is expected";
-        public MvxFormsViewModel()
-        {
-        }
+        private const string ApiKey = "03e8168ffbb8cafb6b8b6679c528ec97";
 
-        public override Task Initialize()
-        {
-            //TODO: Add starting logic here
+        private const string Text = "Today rain is expected";
 
-            return base.Initialize();
-        }
+        private const string CelsiusSign = "°C";
 
-        public IMvxCommand GetWeatherCommand => new MvxCommand(GetWeather);
+        private const string DateKey = "Date";
+
+        public IMvxAsyncCommand GetWeatherCommand => new MvxAsyncCommand(GetWeatherAsync);
 
         private string city;
+        private string highTemp;
+        private string lowTemp;
+        private string description;
+        private string message;
+
         public string City
         {
-            get { return city; }
+            get => city;
             set { SetProperty(ref city, value); }
         }
-        private string highTemp;
+
         public string HighTemp
         {
-            get { return highTemp; }
+            get => highTemp;
             set { SetProperty(ref highTemp, value); }
         }
 
-        private string lowTemp;
         public string LowTemp
         {
-            get { return lowTemp; }
+            get => lowTemp;
             set { SetProperty(ref lowTemp, value); }
         }
 
-        private string description;
         public string Description
         {
-            get { return description; }
+            get => description;
             set { SetProperty(ref description, value); }
         }
 
-        private string messege;
-        public string Messege
+        public string Message
         {
-            get { return messege; }
-            set { SetProperty(ref messege, value); }
+            get => message;
+            set { SetProperty(ref message, value); }
         }
 
-        private int Celcius(double calvin)
+        private async Task GetWeatherAsync()
         {
-            return (int)calvin - 273;
-
-        }
-
-        private async void GetWeather()
-        {
-
             try
             {
-                var api_key = "03e8168ffbb8cafb6b8b6679c528ec97";
+                var cityText = City;
+                var Url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", cityText, ApiKey);
 
-                var citytext = City;
-                Messege = "";
-
-                string URL = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", citytext, api_key);
-
-                HttpClient httpClient = new HttpClient();
-                var response = await httpClient.GetAsync(URL);
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(Url);
+                Message = string.Empty;
 
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
                     var weatherList = JsonConvert.DeserializeObject<RootObject>(content);
 
-                    HighTemp = Celcius(weatherList.main.temp_max).ToString() + "°C";
-                    LowTemp = Celcius(weatherList.main.temp_min).ToString() + "°C";
+                    HighTemp = $"{Celcius(weatherList.main.temp_max)} {CelsiusSign}";
+                    LowTemp = $"{Celcius(weatherList.main.temp_min)} {CelsiusSign}";
                     Description = weatherList.weather[0].description;
-                    //Xamarin.Forms.Application.Current.Properties.Remove("Date");
-                    if (weatherList.rain!=null) // we have data about rain 
+
+                    if (weatherList.rain != null) // we have data about rain 
                     { 
-                        if (!Xamarin.Forms.Application.Current.Properties.ContainsKey("Date")) // add new key and value
+                        if (!Xamarin.Forms.Application.Current.Properties.ContainsKey(DateKey)) // add new key and value
                         {
-                            Xamarin.Forms.Application.Current.Properties["Date"] = DateTime.Now.Date;
-                            Messege = text; // show note about reain 
+                            Xamarin.Forms.Application.Current.Properties[DateKey] = DateTime.Now.Date.ToUniversalTime();
+                            Message = Text; // show note about rain 
                         }
                         else   // value is already exist 
                         {
-                            var dateString = Xamarin.Forms.Application.Current.Properties["Date"];
-                            var dateValue = DateTime.Parse(dateString.ToString());
+                            var dateString = Xamarin.Forms.Application.Current.Properties[DateKey].ToString();
+                            var dateValue = DateTime.Parse(dateString);
 
-                            if (dateValue != DateTime.Now.Date)  // data has been expired 
+                            if (dateValue != DateTime.Now.Date.ToUniversalTime())  // data has been expired 
                             {
-                                Xamarin.Forms.Application.Current.Properties["Date"] = DateTime.Now.Date; // refresh data 
-                                Messege = text; // show note about reain 
+                                Xamarin.Forms.Application.Current.Properties[DateKey] = DateTime.Now.Date; // refresh data 
+                                Message = Text; // show note about rain 
                             }
-
                         }
                     }
                 }
@@ -116,7 +102,9 @@ namespace MvxForms.Core.ViewModels
             }
         }
 
+        private int Celcius(double calvin)
+        {
+            return (int)calvin - 273;
+        }
     }
-
 }
-
